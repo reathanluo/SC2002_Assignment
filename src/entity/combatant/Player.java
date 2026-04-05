7181a1ab0eb4fd06f93225b181ab1facd049095d
reathanluo <72963379+reathanluo@users.noreply.github.com>
2026-04-05 13:43:38 +0800

    implemented player abstract class

---
+++ src/entity/combatant/Player.java
@@ -0,0 +1,61 @@
+package entity.combatant;
+
+
+import entity.action.Action;
+import entity.item.Item;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// abstract player class to implement specific classes
+public abstract class Player extends Combatant {
+    private List<Item> inventory;
+    private int specialSkillCooldown;
+
+    public Player(String name, int hp, int attack, int defense, int speed) {
+        super(name, hp, attack, defense, speed);
+        this.inventory = new ArrayList<>();
+        this.specialSkillCooldown = 0;
+    }
+
+    public List<Item> getInventory() {
+        return Collections.unmodifiableList(inventory);
+    }
+
+    public int getSpecialSkillCooldown() {
+        return specialSkillCooldown;
+    }
+
+    public void setSpecialSkillCooldown(int cooldown) {
+        this.specialSkillCooldown = cooldown;
+    }
+
+    public void decrementCooldown() {
+        if (specialSkillCooldown > 0) {
+            specialSkillCooldown--;
+        }
+    }
+
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+
+    public void removeItem(Item item) {
+        inventory.remove(item);
+    }
+
+    // returns true if inventory contains at least one item not used yet
+    public boolean hasItems() {
+        for (Item item : inventory) {
+            if (!item.isConsumed())
+                return true;
+        }
+        return false;
+    }
+
+    public abstract Action getSpecialSkill();
+
+    // called when player kills an enemy during special skill
+    public void onKill() {
+    }
+}
