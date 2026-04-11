a2f0462ad93bd37274fba0d2c38be1d57233120c
reathanluo <72963379+reathanluo@users.noreply.github.com>
2026-04-11 11:33:47 +0800

    fix the bug that FireFlask does nothing

--- src/entity/item/FireFlask.java
+++ src/entity/item/FireFlask.java
@@ -1,5 +1,6 @@
 package entity.item;
 
+import entity.action.Action;
 import entity.combatant.Combatant;
 import entity.combatant.Player;
 import entity.effect.BurnEffect;
@@ -16,11 +17,23 @@ public class FireFlask extends Item {
         return "Hurl fire at one enemy. Burns for 20 damage/turn for 3 turns (bypasses DEF).";
     }
 
+    // delegates target resolution to a single-target action so BattleEngine prompts the player to pick an enemy
+    @Override
+    public Action getTargetResolutionAction(Player user) {
+        return new Action() {
+            @Override public String getName() { return "Fire Flask"; }
+            @Override public String getDescription(Combatant p) { return ""; }
+            @Override public void execute(Combatant p, List<Combatant> t) {}
+            @Override public boolean isAvailable(Combatant p) { return true; }
+            @Override public boolean requiresTargetSelection() { return true; }
+            @Override public boolean isAreaOfEffect() { return false; }
+        };
+    }
+
     @Override
     public void use(Player user, List<Combatant> targets) {
         for (Combatant target : targets) {
             target.addStatusEffect(new BurnEffect(3, 20));
-            System.out.println("  " + user.getName() + " hurls a Fire Flask at " + target.getName() + "!");
         }
         markConsumed();
     }
